97b595d76f3f056eea754f9667ff4af9d264177e
sishariful <si254691@gmail.com>
2024-02-19 21:51:27 +0600

    added some js function

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-const allBtn = document.getElementsByClassName('A');
-let count = 0;
-for (const btn of allBtn){
-    btn.addEventListener('click' , function(e){
-        
-        count = count + 1;
-        const elementName = e.target.innerText;
-        const priceContainer = document.getElementById('price');
-        const p = document.createElement('h1');
-        p.innerText = elementName;
-        const p2 = document.createElement('p');
-        p2.innerText = 'Economoy';
-        const p3 = document.createElement('p');
-        p3.innerText = 550;
-
-        priceContainer.appendChild(p);
-        priceContainer.appendChild(p2);
-        priceContainer.appendChild(p3);
-
-        document.getElementById('total-price');
-        const multi = count * 550;
-        document.getElementById('grand-total');
-        const multi2 = count * 550 ;
-
-        showDisplay('grand-total' , multi2)
-        showDisplay('total-price' , multi)
-        showDisplay("count-tricket" , count);
+function scrollsection(){
+    const target = document.getElementById('target')
+    target.scrollIntoView({
+        behavior:'smooth'
     })
 }
+
+let seatLeft=40;
+let seatBook=0;
+let totalprice=0;
+let grandprice=0;
+let discountprice=0;
+const allBtn = document.getElementsByClassName('A')
+console.log(allBtn);
+    for(const btn of allBtn){
+        console.log(btn)
+        btn.addEventListener('click', function(){
+           
+          
+            seatLeft--;
+            seatBook++;
+            if(seatBook >= 5){
+                alert("You don't buy no more ticket ")
+              return;
+            }  
+           
+            btn.style.backgroundColor = '#1DD100';
+            SetInnerText('remaning-seat',seatLeft);
+            SetInnerText('count-tricket',seatBook);
+            btn.setAttribute('disabled',true);
+            // append child taable
+            const ticektsection = document.getElementById('price')
+            const p = document.createElement('p');
+            p.innerText =btn.innerText;
+            const p1 = document.createElement('p');
+            p1.innerText="Economy";
+            const p2 = document.createElement('p');
+           
+            p2.innerText=550;
+            ticektsection.appendChild(p);
+            ticektsection.appendChild(p1);
+            ticektsection.appendChild(p2);
+
+            
+
+            totalprice += 550;
+            SetInnerText('total-price',totalprice);
+            grandprice=totalprice;
+            SetInnerText('grand-total',grandprice); 
+          
+
+            
+        })
+    }

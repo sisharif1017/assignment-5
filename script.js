178abc0e1b1222110ca6678f10fb9a7d6789aecd
sishariful <si254691@gmail.com>
2024-02-20 12:01:57 +0600

    complete all

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function scrollsection(){
+function bannersection(){
     const target = document.getElementById('target')
     target.scrollIntoView({
         behavior:'smooth'
     })
 }
 
-let seatLeft=40;
-let seatBook=0;
-let totalprice=0;
-let grandprice=0;
-let discountprice=0;
-const allBtn = document.getElementsByClassName('A')
-console.log(allBtn);
-    for(const btn of allBtn){
+let seatLeft = 40;
+let seatBook = 0;
+let totalprice = 0;
+let grandprice = 0;
+let discountprice = 0;
+const allButton = document.getElementsByClassName('A')
+console.log(allButton);
+    for(const btn of allButton){
         console.log(btn)
         btn.addEventListener('click', function(){
            
@@ -20,7 +20,7 @@ console.log(allBtn);
             seatLeft--;
             seatBook++;
             if(seatBook >= 5){
-                alert("You don't buy no more ticket ")
+                alert("You don't buy any more ticket.Thank You Sir!")
               return;
             }  
            
@@ -28,18 +28,18 @@ console.log(allBtn);
             SetInnerText('remaning-seat',seatLeft);
             SetInnerText('count-tricket',seatBook);
             btn.setAttribute('disabled',true);
-            // append child taable
+            
             const ticektsection = document.getElementById('price')
-            const p = document.createElement('p');
-            p.innerText =btn.innerText;
-            const p1 = document.createElement('p');
-            p1.innerText="Economy";
-            const p2 = document.createElement('p');
+            const a = document.createElement('a');
+            a.innerText =btn.innerText;
+            const a1 = document.createElement('a');
+            a1.innerText="Economy";
+            const a2 = document.createElement('a');
            
-            p2.innerText=550;
-            ticektsection.appendChild(p);
-            ticektsection.appendChild(p1);
-            ticektsection.appendChild(p2);
+            a2.innerText=550;
+            ticektsection.appendChild(a);
+            ticektsection.appendChild(a1);
+            ticektsection.appendChild(a2);
 
             
 
@@ -51,4 +51,80 @@ console.log(allBtn);
 
             
         })
+    }
+    
+    const btn = document.getElementById("apply");
+
+    
+    btn.addEventListener('click',function(){
+        
+      
+        if(seatBook >= 4) {
+            const apply = document.getElementById('apply')
+           
+         const couponCode = document.getElementById('field').value;
+         if(couponCode === "NEW15"){
+            
+            discountprice = totalprice * 0.15;
+            grandprice = totalprice - discountprice;
+            SetInnerText('grand-total',grandprice);
+            const discountPrice = document.getElementById('discount-price');
+            const a = document.createElement('a');
+            a.innerText = "Discount Price"
+            discountPrice.appendChild(a);
+            const a1 = document.createElement('a');
+            a1.innerText = "BDT"+ " " + discountprice.toFixed(2);
+            discountPrice.appendChild(a1);
+            
+            const hideDiv = document.getElementById('hide-div')
+            hideDiv.classList.add('hidden')
+            
+         }
+         else if(couponCode === "Couple 20"){
+            
+            discountprice = totalprice * 0.20;
+            grandprice = totalprice - discountprice;
+            SetInnerText('grand-total',grandprice);
+            const discountPrice = document.getElementById('discount-price');
+            const a = document.createElement('a');
+            a.innerText = "Discount Price"
+            discountPrice.appendChild(a);
+            const a1 = document.createElement('a');
+            a1.innerText = "BDT"+ " " + discountprice.toFixed(2);
+            discountPrice.appendChild(a1);
+            
+            const hideDiv = document.getElementById('hide-div')
+            hideDiv.classList.add('hidden')
+
+         }else{
+            alert('This is Wrong Coupon.')
+         }
+
+            
+        }
+
+    })
+   
+
+    const btn2 = document.getElementById("next");
+    btn2.addEventListener('click', function () {
+    const num = document.getElementById("phone-number").value;
+    if (seatBook >= 1 && num.length >= 1) {
+        getHidden('main-element');
+        getHidden('main-element2');
+        getAdd('success');
+    }
+})
+
+
+
+    
+    function getHidden(elementId){
+        document.getElementById(elementId).classList.add('hidden')
+    }
+    function getAdd(elementId){
+        document.getElementById(elementId).classList.remove('hidden');
+    }
+    function SetInnerText(elementId,value){
+        document.getElementById(elementId).innerText = value;
     }
